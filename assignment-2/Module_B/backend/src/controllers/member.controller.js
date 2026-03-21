397b57ecd226d4e34703a14cc93f23b2d8f52ed0
Pushkar Modi <116958355+Pushkar111@users.noreply.github.com>
2026-03-21 22:06:09 +0530

    feat: include user context in member service calls for enhanced security

--- assignment-2/Module_B/backend/src/controllers/member.controller.js
+++ assignment-2/Module_B/backend/src/controllers/member.controller.js
@@ -6,14 +6,14 @@ const { ACTIONS }          = require('../utils/constants');
 
 async function getAll(req, res, next) {
   try {
-    const result = await memberService.getAll(req.query);
+    const result = await memberService.getAll(req.user, req.query);
     return sendSuccess(res, { members: result.members }, 200, result.pagination);
   } catch (err) { next(err); }
 }
 
 async function getById(req, res, next) {
   try {
-    const member = await memberService.getById(req.params.id);
+    const member = await memberService.getById(req.params.id, req.user);
     return sendSuccess(res, { member });
   } catch (err) { next(err); }
 }
@@ -28,7 +28,7 @@ async function create(req, res, next) {
 
 async function update(req, res, next) {
   try {
-    const old    = await memberService.getById(req.params.id);
+    const old    = await memberService.getById(req.params.id, req.user);
     const member = await memberService.update(req.params.id, req.body);
     await writeAuditRecord({ user: req.user, tableName: 'Member', recordId: Number(req.params.id), action: ACTIONS.UPDATE, oldValue: old, newValue: req.body, method: req.method, endpoint: req.originalUrl, statusCode: 200, ip: req.ip });
     return sendSuccess(res, { member });
@@ -37,7 +37,7 @@ async function update(req, res, next) {
 
 async function deleteMember(req, res, next) {
   try {
-    const old    = await memberService.getById(req.params.id);
+    const old    = await memberService.getById(req.params.id, req.user);
     const result = await memberService.delete(req.params.id);
     await writeAuditRecord({ user: req.user, tableName: 'Member', recordId: Number(req.params.id), action: ACTIONS.DELETE, oldValue: old, method: req.method, endpoint: req.originalUrl, statusCode: 200, ip: req.ip });
     return sendSuccess(res, result);
